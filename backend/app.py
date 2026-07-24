d8f7c31cff69b845c333d03cae4e9813c32a5bb0
mennatullahelshahawy <mennatullah.elshahawy@gmail.com>
2026-07-24 06:06:09 +0300

    adjusted backend to load env

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,24 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
+import os
+from pathlib import Path  
+from dotenv import load_dotenv
+
+base_dir = Path(__file__).resolve().parent.parent
+env_path = base_dir / '.env'
+
+load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
 CORS(app)
 
-db = MongoClient("mongodb://root:pass@mongodb:27017/quotes-app?authSource=admin")["quotes-app"]
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASS")
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+
+db = MongoClient(f"mongodb://{db_user}:{db_pass}@{db_host}:27017/{db_name}?authSource=admin")[db_name]
 
 @app.route('/api/quote')
 def get_quote():
